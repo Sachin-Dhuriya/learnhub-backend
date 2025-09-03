a8e9dedc872c2aebdbce494689d39ccdca710547
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2025-09-03 11:47:51 +0530

    Auth middleware , Login and profile api implimented

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+const authenticate = (req,res,next) =>{
+    const token = req.headers.authorization?.split(" ")[1];
+    if(!token){
+        return res.status(401).json({message: 'Please login first..!!!'})
+    }
+
+    try {
+        let decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded;
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({message: 'Invalid token'})
+    }
+
+}
+
+export default authenticate;
